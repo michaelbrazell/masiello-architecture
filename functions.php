b5c5b57481cff18ba48efc5a4d965bbc9f52b54b
Mike Murray <michaelbrazell@gmail.com>
2017-11-15 22:12:57 -0500

    Add support for image overlay text to design work items

--- functions.php
+++ functions.php
@@ -4,14 +4,10 @@
 --------------------------------------------------------*/
  
 function divichild_enqueue_styles() {
-
   $parent_style = 'parent-style';
-
   wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-  wp_enqueue_style( 'child-style',
-  get_stylesheet_directory_uri() . '/style.css',
-  array( $parent_style ),
-  wp_get_theme()->get('Version')
-  );
+  wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ), wp_get_theme()->get('Version') );
 }
 add_action( 'wp_enqueue_scripts', 'divichild_enqueue_styles' );
+
+wp_enqueue_script( 'script', get_stylesheet_directory_uri() . '/js/masiello.js', array ( 'jquery' ), 1.0, true);
---
+++ js/masiello.js
@@ -0,0 +1,18 @@
+(function($) {
+	
+  // Use Image Title as Overlay
+  $('.et_pb_image').each( function() {
+    var imageContainer = this;
+    $(this).find('img').each(function(){
+      var titleAttr = $(this).attr('title');
+      if (titleAttr != undefined) {
+        var titleAttrHTML = '<span class="design_work_title">';
+        titleAttrHTML += titleAttr;
+        titleAttrHTML += '</span>';
+        $(imageContainer).find('a').append(titleAttrHTML);
+        console.log(titleAttr);
+      }
+    })
+  });
+
+})( jQuery );
